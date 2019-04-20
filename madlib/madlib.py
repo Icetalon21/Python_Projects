4465707a94ada99fe6f46bab859b41859faa8055
Emilia Huerta <39370518+Icetalon21@users.noreply.github.com>
2019-04-20 03:30:41 -0700

    Create madlib.py

---
+++ madlib/madlib.py
@@ -0,0 +1,8 @@
+#madlib.py
+#Emilia Huerta
+
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adverb = input("Enter an adverb: ")
+print("A", adjective, noun, "should never", verb, adverb)
