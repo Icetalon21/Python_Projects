22b0eda8d3381d52c57c3d285478fc70e35ada5a
Emilia Huerta <39370518+Icetalon21@users.noreply.github.com>
2019-04-20 03:36:21 -0700

    Create sphere.py

---
+++ sphere/sphere.py
@@ -0,0 +1,5 @@
+from math import pi
+r = 12
+volume = (4/3) * pi * r ** 3
+surface_area = 4 * pi * r ** 2
+print("The volume of a sphere with radius", r, "is", volume, "and the surface area is", surface_area)
